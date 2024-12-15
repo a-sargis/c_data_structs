5abf10ce298571dc9b714358ca3a25d3d5c7e520
asargis <asargis@live.com>
2024-12-16 03:33:47 +0700

    first commit

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <assert.h>
+#include "stack.h"
+
+int
+main(void)
+{
+    Stack* stack = Stack_new(11);
+
+    stack->push(stack, 22);
+    stack->push(stack, 33);
+    stack->push(stack, 44);
+
+    assert("peek passed" && 44 == stack->peek(stack));
+
+    stack->print(stack);
+
+    assert("size passed" && 4 == stack->size(stack));
+
+    stack->pop(stack);
+
+    assert("pop passed" && 3 == stack->size(stack));
+
+    stack->destroy(stack);
+
+    return 0;
+}
---
+++ stack.h
@@ -0,0 +1,108 @@
+//
+// Created by sargis on 16.12.2024.
+//
+
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifndef STACK_H
+#define STACK_H
+
+typedef struct Stack Stack;
+
+struct Stack
+{
+    int    data;
+    Stack* prev;
+
+    void (*push)  (Stack* self, int data);
+    void (*pop)   (Stack* self);
+    int  (*peek)  (Stack* self);
+    void (*print) (Stack* self);
+    int  (*size)  (Stack* self);
+    void (*destroy)  (Stack* self);
+};
+
+void
+push(Stack* self, int data)
+{
+    Stack* prevElem = (Stack*)malloc(sizeof(Stack));
+
+    *prevElem = *self;
+    self->data = data;
+    self->prev = prevElem;
+}
+
+void
+pop(Stack* self)
+{
+    *self = *self->prev;
+}
+
+int
+peek(Stack* self)
+{
+    return self->data;
+}
+
+void
+print(Stack* self)
+{
+    Stack* temp = self;
+
+    while (temp != nullptr)
+    {
+        printf("%d\n", temp->data);
+        temp = temp->prev;
+    }
+}
+
+int
+size(Stack* self)
+{
+    Stack* temp = self;
+    int size = 0;
+
+    while (temp != nullptr)
+    {
+        size += 1;
+        temp = temp->prev;
+    }
+
+    return size;
+}
+
+void destroy(Stack* stack)
+{
+    Stack* temp;
+
+    while (stack != NULL)
+    {
+        temp = stack;
+        stack = stack->prev;
+        free(temp);
+    }
+
+}
+
+Stack*
+Stack_new(int data)
+{
+    Stack* stack = (Stack*)malloc(sizeof(Stack));
+
+    stack->data = data;
+    stack->prev = nullptr;
+
+    stack->push     = push;
+    stack->peek     = peek;
+    stack->pop      = pop;
+    stack->print    = print;
+    stack->size     = size;
+    stack->destroy  = destroy;
+
+    return stack;
+}
+
+#endif //STACK_H
